3a01a606268a00c03fb4d780b5e495eb0bdcd86d
HumbertoLuiz <kaos_132@hotmail.com>
2022-01-14 20:47:41 -0300

    Routes

--- frontend/src/custom.d.ts
+++ frontend/src/custom.d.ts
@@ -4,3 +4,17 @@ declare module '*.svg' {
    const src: string;
   export default src;
 }
+
+declare module '*.scss' {
+  const css: { [key: string]: string };
+  export default css;
+}
+declare module '*.sass' {
+  const css: { [key: string]: string };
+  export default css;
+}
+declare module 'react-markup';
+declare module '*.webp';
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
