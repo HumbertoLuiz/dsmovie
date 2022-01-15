1325bb293cb6c587af1eea07409eb505b98b6540
HumbertoLuiz <kaos_132@hotmail.com>
2022-01-14 22:20:15 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,11 +1,31 @@
-import Pagination from 'components/Pagination';
-
+import Pagination from "components/Pagination";
+import MovieCard from "components/MovieCard";
 
 function Listing() {
-
-	return (
-		< Pagination />
-	);
+  return (
+    <>
+      <Pagination />
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Listing;
