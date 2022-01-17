dcb45a8440b5167965675e7691c9654a5c3ced49
HumbertoLuiz <kaos_132@hotmail.com>
2022-01-17 17:29:25 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -7,7 +7,7 @@ import { MoviePage } from "types/movie";
 
 function Listing() {
 
-	const [pageNumber, setPageNumber] = useState();
+	const [pageNumber, setPageNumber] = useState(0);
 
 	const [page, setPage] = useState<MoviePage>({
 		content: [],
@@ -29,9 +29,13 @@ function Listing() {
 		});
 	}, [pageNumber]);
 
+	const handlePageChange = (newPageNumber : number) => {
+		setPageNumber(newPageNumber);
+	}
+
   return (
     <>
-      <Pagination />
+      <Pagination page={page} onChange={handlePageChange}/>
       <div className="container">
         <div className="row">
 
